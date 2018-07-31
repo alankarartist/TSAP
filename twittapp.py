2d7b53b79eba7e4ea13510f3deeec10512c7c77c
Alankar Singh <32199681+starboyalankarartist@users.noreply.github.com>
2018-07-31 15:02:58 +0530

    Update twittapp.py

--- twittapp.py
+++ twittapp.py
@@ -10,25 +10,25 @@ auth.set_access_token(access_token, access_token_secret)
 api = tweepy.API(auth)
 subject = input('Enter the keyword that you want to search ')
 list = []
-neutral=0
-positive=0
-negative=0
-count =0
+neu=0
+pos=0
+neg=0
+c =0
 for tweet in tweepy.Cursor(api.search, q=subject).items(1000):
     print(tweet.text)
     analysis = TextBlob(tweet.text)
     print(analysis.sentiment)
     list.append(analysis.sentiment.polarity)
-    count+=1
+    c+=1
 print ("Average Polarity" + str(mean(list)) )   
 for i in list:
     if i==0.0:
-       neutral+=1
+       neu+=1
     if i>0.0:
-       positive+=1
+       pos+=1
     if i<0.0:
-       negative+=1
-print ("Total Neutral :" + str(neutral))
-print ("Total Positive :" + str(positive))
-print("Total Negative :" + str(negative))
-print("Total Tweets Searched" + str(count))
+       neg+=1
+print ("Total Neutral :" + str(neu))
+print ("Total Positive :" + str(pos))
+print("Total Negative :" + str(neg))
+print("Total Tweets Searched" + str(c))
